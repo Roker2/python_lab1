bb7b8e359858ae0b1fcded17a7dfc53bea7b6dc4
Roker2 <dimaminko2000@gmail.com>
2020-02-16 19:09:07 +0300

    Add four program

--- main.py
+++ main.py
@@ -38,6 +38,37 @@ def quicksort(data, left, right):
         quicksort(data, p + 1, right)
 
 
+def merge(left_data, right_data):
+    sorted_data = []
+    left_index = 0
+    right_index = 0
+    for _ in range(len(left_data) + len(right_data)):
+        if (left_index < len(left_data)) & (right_index < len(right_data)):
+            if left_data[left_index] <= right_data[right_index]:
+                sorted_data.append(left_data[left_index])
+                left_index += 1
+            else:
+                sorted_data.append(right_data[right_index])
+                right_index += 1
+        elif left_index == len(left_data):
+            for value in right_data[right_data:]:
+                sorted_data.append(value)
+            break
+        elif right_index == len(right_data):
+            for value in left_data[left_index:]:
+                sorted_data.append(value)
+            break
+    return sorted_data
+
+
+def merge_sort(data):
+    if len(data) <= 1:
+        return data
+    center = len(data) // 2
+    return merge(merge_sort(data[:center]),
+                 merge_sort(data[center:]))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--type', type=str, help='Type program: one, two')
@@ -101,3 +132,14 @@ if __name__ == '__main__':
         print(mas)
         quicksort(mas, 0, len(mas) - 1)
         print(mas)
+    # print(mas[:(len(mas) // 2)])
+
+    if vars(parser.parse_args())['type'] == 'four':
+        file = open(vars(parser.parse_args())['file'], 'r')
+        string = file.read()
+        file.close()
+        mas = []
+        for value in string.split():
+            mas.append(int(value))
+        print(mas)
+        print(merge_sort(mas))
